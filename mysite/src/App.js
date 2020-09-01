48894714f4b0fef9088e59029096e897c6b2f642
Carlo Messere <messerecarlo27@gmail.com>
2020-09-01 22:27:41 +0200

    created and styled header component

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <Header></Header>
     </div>
   );
+}
+
+const Header = () => {
+  return(
+    <div className="App-header"> 
+      <div className="header-content">ABOUT</div>
+      <div className="header-content">WORK</div>
+      <div className="header-content">BLOG</div>
+      <div className="header-content">CONTACT</div>
+    </div>
+  )
 }
 
 export default App;

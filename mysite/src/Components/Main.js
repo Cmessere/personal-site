8ecd32776ae027aa549d5c97fe88035306b6fd17
Carlo Messere <messerecarlo27@gmail.com>
2020-09-03 23:28:59 +0200

    clickable div

--- mysite/src/Components/Main.js
+++ mysite/src/Components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Main.css';
+import { useHistory } from 'react-router';
 
 export const Main = () => {
+  let history = useHistory()
   return (
           <div className="main-page">
-                <div className="portrait">
+                <div className="portrait" onClick={()=>history.push("/about")}>
                   <div className="inner-portrait">
                     <div className="title">
                       <p className="name"> Carlo Messere</p>

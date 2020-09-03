4152b8f9bcfbca656050ed7c269fba8e79401afd
Carlo Messere <messerecarlo27@gmail.com>
2020-09-03 14:11:57 +0200

    changed main page

--- mysite/src/Components/Main.js
+++ mysite/src/Components/Main.js
@@ -5,19 +5,14 @@ import './App.css';
 export const Main = () => {
   return (
           <div className="main-page">
-            <div className="main-page-column">
-              <div className="photo-div">
-                <img className="portrait" src={photo} alt="photo" />
+                <div className="portrait">
+                  <div className="inner-portrait">
+                    <div className="title">
+                      <p className="name"> Carlo Messere</p>
+                      <p className="work"> Software Developer</p>
+                    </div>
+                  </div>
               </div>
-              <div className="main-div">
-                <h1 className="name">Carlo Messere <h1 className="profession">Software Developer</h1> </h1>
-              </div>
-            </div>
-            <div className="main-page-column">
-              <div className="footer">
-                <h1 className="content">"I make stuff"</h1>
-              </div>
-            </div>
           </div>
   );
 };

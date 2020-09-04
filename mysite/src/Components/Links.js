f42bf7ee9053e76be33c55a2a7934a2fedc4bb02
Carlo Messere <messerecarlo27@gmail.com>
2020-09-05 01:24:42 +0200

    created link bubble

--- mysite/src/Components/Links.js
+++ mysite/src/Components/Links.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "./Links.css"
 
 export const Links = () => {
   return (
     <div className="Link-page">
-
+      <h1>Here you can find all the way to get in touch with me</h1>
+      <div className="contact">
+        <div className="contact-bubble">
+          <div className="inner-contact-bubble">
+            <a className="bubble-link" href="https://www.linkedin.com/in/carlo-messere-65a193115/" rel="noopener noreferrer" target="_blank">
+              <FontAwesomeIcon className="bubble-icon" icon={faLinkedin} />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

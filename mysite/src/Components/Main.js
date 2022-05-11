db7e586f7ef66200ce248d75c10b468d7b55634a
cmessere <carlo.messere@accenture.com>
2022-05-12 00:24:12 +0200

    added link

--- mysite/src/Components/Main.js
+++ mysite/src/Components/Main.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
+import { Link } from "react-scroll";
 
 export const Main = () => {
   return (
-          <div className="main-page">
-                <div className="portrait" onClick={()=> console.log("yo")}>
-                  <div className="inner-portrait">
-                    <div className="title">
-                      <p className="name"> START</p>
-                    </div>
-                  </div>
-              </div>
+    <div className="main-page">
+      <Link
+        activeClass="active"
+        to="about-page"
+        spy={true}
+        smooth={true}
+        duration={1000}
+      >
+        <div className="portrait" onClick={() => console.log("yo")}>
+          <div className="inner-portrait">
+            <div className="title">
+              <p className="name">START</p>
+            </div>
           </div>
+        </div>
+      </Link>
+    </div>
   );
 };

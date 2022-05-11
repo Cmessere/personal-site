585a023846b209da9e394cec1fdaa1ac7a2bf908
cmessere <carlo.messere@accenture.com>
2022-05-12 00:48:35 +0200

    Updated about

--- mysite/src/Components/About.js
+++ mysite/src/Components/About.js
@@ -2,40 +2,43 @@ import React from 'react';
 import './About.css';
 import './AboutAnimation.scss';
 import photo from './images/photo.png';
-import { useHistory } from 'react-router';
+import { Link } from "react-scroll";
 
 export const About = () => {
-  let history = useHistory();
 
   return (
     <div className="about-page">
-      <h1 className="about-title">Hi I am Carlo, Welcome to my site!</h1>
       <img className="curve" src={photo} alt="curve"></img>
+      <h1 className="about-title">Hey There, Welcome to my site!</h1>
       <p className="about-paragraph">
-        I've always wanted to work in the gaming industry, but I could not decide wether as an artist, a designer or even as a voice actor!
+        My Name is Carlo Messere, and I am a software developer.
       </p>
       <p className="about-paragraph">
         One day when I was 14 I picked up an old copy of Deitel's "C How to program" and instantly
-        fell in love with coding.
+        fell in love with it.
       </p>
       <p className="about-paragraph">
-        Soon later I moved to C++, my favourite programming language to date.
-        After highscool I started University and opted for Computer Engineering,
-        because I liked the idea of an extensive Math and Physics background.
+        Coding became my passion, and I soon decided to make it a profession. I got my Master's in Computer Engineering that gave me a structured approach and the forma mentis to work as an engineer.
+              </p>
+      <p className="about-paragraph">
+        All the while I kept busy tinkering with different languages and technologies.
+        To this day I have good experience with multiple programming languages, spacing from the "Old reliables" like C/C++, to the scripting oriented like Javascript, Python and Rust.
       </p>
       <p className="about-paragraph">
-        Through classes I learned a variety of programming concepts, languages and techniques,
-        and how to apply them in different contexts.
-        All the while in my free time I started tinkering more with engines and libraries,
-        developing simple games as a hobby.
+        I live and breathe technologies, and I always love to learn!
       </p>
+      <Link
+        activeClass="active"
+        to="link-page"
+        spy={true}
+        smooth={true}
+        duration={1000}
+      >
       <p className="about-paragraph">
-        After graduating I started working in the Development Team of my current company.
-        As of now I am specialized in Front-End and Web development, in particular with React.js and Office.js. 
-      </p>
-      <p className="about-paragraph" onClick={() => history.push("links")}>
         Click here if you want to get in contact!
       </p>
+      </Link>
+
     </div>
   );
 };

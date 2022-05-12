c9dee3fdd4c86395f8b5b3eab2f0033cefded4be
cmessere <carlo.messere@accenture.com>
2022-05-12 23:30:23 +0200

    multiple changes

--- mysite/src/Components/Links.js
+++ mysite/src/Components/Links.js
@@ -30,7 +30,6 @@ export const Links = () => {
           <h1 className="contact-name">My CV</h1>
         </div>
         <div className="link-component">
-          <h1 className="contact-name">Apps</h1>
           <div className="contact-bubble">
             <div className="inner-contact-bubble">
               <a
@@ -43,9 +42,8 @@ export const Links = () => {
               </a>
             </div>
           </div>
+          <h1 className="contact-name">Apps</h1>
         </div>
-      </div>
-      <div className="link-row">
         <div className="link-component">
           <div className="contact-bubble">
             <div className="inner-contact-bubble">
@@ -54,15 +52,16 @@ export const Links = () => {
                 href="https://www.linkedin.com/in/carlo-messere-65a193115/"
                 rel="noopener noreferrer"
                 target="_blank"
-              >
+                >
                 <FontAwesomeIcon className="bubble-icon" icon={faLinkedin} />
               </a>
             </div>
           </div>
           <h1 className="contact-name">Linkedin</h1>
         </div>
+        </div>
+        <div className="link-row">  
         <div className="link-component">
-          <h1 className="contact-name">GitHub</h1>
           <div className="contact-bubble">
             <div className="inner-contact-bubble">
               <a
@@ -75,9 +74,8 @@ export const Links = () => {
               </a>
             </div>
           </div>
+          <h1 className="contact-name">GitHub</h1>
         </div>
-      </div>
-      <div className="link-row">
         <div className="link-component">
           <div className="contact-bubble">
             <div className="inner-contact-bubble">
@@ -94,7 +92,6 @@ export const Links = () => {
           <h1 className="contact-name">Hacker Rank</h1>
         </div>
         <div className="link-component">
-          <h1 className="contact-name">Email Me</h1>
           <div className="contact-bubble">
             <div className="inner-contact-bubble">
               <a
@@ -105,6 +102,7 @@ export const Links = () => {
               </a>
             </div>
           </div>
+      <h1 className="contact-name">Email Me</h1>
         </div>
       </div>
     </div>

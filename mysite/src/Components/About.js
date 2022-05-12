f7b01fa5c92580f25ca99e3d9f551b6eeaa83b7c
cmessere <carlo.messere@accenture.com>
2022-05-12 23:23:45 +0200

    changed class and fixed style

--- mysite/src/Components/About.js
+++ mysite/src/Components/About.js
@@ -9,25 +9,23 @@ export const About = () => {
     <div className="about-page">
       <img className="curve" src={photo} alt="curve"></img>
       <h1 className="about-title">Hey There, Welcome to my site!</h1>
-      <p className="about-paragraph">
+      <p className="paragraph">
         My Name is Carlo Messere, and I am a software developer.
       </p>
-      <p className="about-paragraph">
+      <p className="paragraph">
         One day when I was 14 I picked up an old copy of Deitel's "C How to program" and instantly
         fell in love with it.
       </p>
-      <p className="about-paragraph">
+      <p className="paragraph">
         Coding became my passion, and I soon decided to make it a profession. I got my Master's in Computer Engineering that gave me a structured approach and the forma mentis to work as an engineer.
               </p>
-      <p className="about-paragraph">
+      <p className="paragraph">
         All the while I kept busy tinkering with different languages and technologies.
         To this day I have good experience with multiple programming languages, spacing from the "Old reliables" like C/C++, to the scripting oriented like Javascript, Python and Rust.
       </p>
-      <p className="about-paragraph">
+      <p className="paragraph">
         I live and breathe technologies, and I always love to learn!
       </p>
-
-
     </div>
   );
 };
--- mysite/src/Components/Works.js
+++ mysite/src/Components/Works.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from "react-scroll";
+import './Works.css';
 
 export const Works = () => {
 
   return (
     <div className="works-page">
-      <p className="works-paragraph">
+      <p className="paragraph">
         While attending University I kept busy with multiple activities.
         Most notably, I delved in game development publishing <a className='works-link' href="https://github.com/Cesarsk-Dev-Team" rel="noopener noreferrer" target="_blank">four mobile games</a> made with <b>Unity</b> on the PlayStore, and created a game engine from scratch with C++ and SDL.
         <br></br>
         All the while I enjoyed participating in competitive <a className='works-link' href="https://www.hackerrank.com/Shiro27" rel="noopener noreferrer" target="_blank">online coding challenges</a> and deepened my knowledge with Data Structures and popular Algorithms.
       </p>
-      <p className="works-paragraph">
+      <p className="paragraph">
         As a professional developer, I started as a C# backend developer and later moved on to frontend development, specifically with Javascript, Typescipt and React.js.
       </p>
-      <p className="works-paragraph">
+      <p className="paragraph">
         I have developed a <a className='works-link' href="https://www.iconsulting.biz/en/oscar/" rel="noopener noreferrer" target="_blank">product</a> for my former company and worked with some of Italy's biggest companies like Pirelli, Unipol and Generali, developing custom tailored Single Page Applications and Websites with great results.
       </p>
-      <p className="works-paragraph">
+      <p className="paragraph">
         Currently I am working as a consultant on Phillip Morris International's IQOS website.
       </p>
       <Link
@@ -27,7 +28,7 @@ export const Works = () => {
         smooth={true}
         duration={1000}
       >
-      <p className="works-paragraph-last">
+      <p className="paragraph-last">
         Click here if you want to get in contact!
       </p>
       </Link>
